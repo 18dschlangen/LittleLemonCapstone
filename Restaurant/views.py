dd83ce7095023e198c0368c256067d3b646d14e8
18dschlangen <1999dschlangen@gmail.com>
2023-05-12 10:27:32 -0500

    added Booking api routes

--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
 
-# Create your views here.
-def index(request):
-    return render(request, 'newindex.html', {})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
