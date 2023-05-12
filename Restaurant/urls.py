4c89c0ef4c7fd461c1297c95b957fe2d01fc692a
18dschlangen <1999dschlangen@gmail.com>
2023-05-12 16:20:41 -0500

    made changes to serve static content

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -3,6 +3,7 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('', views.static_content),
     path('menu/', views.MenuItemView.as_view(), name='menu_list'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -4,11 +4,11 @@ from .serializers import BookingSerializer, MenuSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 from rest_framework.authtoken.views import obtain_auth_token
+from django.shortcuts import render
 
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-
 
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
@@ -22,3 +22,6 @@ class UserViewSet(viewsets.ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
    permission_classes = [permissions.IsAuthenticated] 
+
+def static_content(request):
+    return render(request, 'index.html')
