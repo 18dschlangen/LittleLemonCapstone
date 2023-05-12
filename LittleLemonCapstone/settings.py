67b1dcc8296dd501a00bd7423a76c4921dab4bd6
18dschlangen <1999dschlangen@gmail.com>
2023-05-12 09:10:14 -0500

    next

--- LittleLemonCapstone/settings.py
+++ LittleLemonCapstone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Restaurant',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'LittleLemonCapstone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,9 +76,14 @@ WSGI_APPLICATION = 'LittleLemonCapstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon', 
+        'USER': 'admin',
+        'PASSWORD': 'employee@123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" }
+}
 }
 
 
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Booking(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.ID} - {self.Name}"
+    
+class Menu(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Title} - ${self.Price}"
